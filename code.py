17d1c1ae7b14843ec0a467ae3e5f318b2c0ee460
himanshupatil208 <73234020+himanshupatil208@users.noreply.github.com>
2020-10-23 21:15:47 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,33 @@
+import PyPDF2
+import pyttsx3
+
+
+def extract_text(filename):
+	
+	pdfFileObj = open(filename, "rb")
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+	mytext = ""
+
+	for pageNum in range(pdfReader.numPages):
+		pageObj = pdfReader.getPage(pageNum)
+		mytext += pageObj.extractText()
+
+	pdfFileObj.close()
+
+	return mytext
+
+
+def speak_text(text):
+	
+	engine = pyttsx3.init()
+	engine.setProperty('rate', 150)
+	engine.setProperty('voice', 'en+m7')
+	engine.say(text)
+	engine.runAndWait()
+
+
+if __name__ == "__main__":
+	text = extract_text("pdf.pdf")
+	print(text)
+	speak_text(text)
